17c24ce0f419a371f08b5601c2a7870f552792ba
PeggyPsi <peggypsychari@gmail.com>
2025-06-01 22:14:54 +0300

    employee detail page get url param

--- src/pages/EmployeeDetailPage/index.tsx
+++ src/pages/EmployeeDetailPage/index.tsx
@@ -1,9 +1,13 @@
-function EmployeeListDetail() {
+import { useParams } from 'react-router';
+
+function EmployeeDetailPage() {
+    const { id } = useParams<{ id: string }>();
+
     return (
         <div>
-            EmployeeDetail
+            EmployeeDetail {id}
         </div>
     );
 }
 
-export default EmployeeListDetail;
+export default EmployeeDetailPage;
